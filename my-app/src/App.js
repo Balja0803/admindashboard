8542d6e49928b7a976b97feae396cb79a542ca39
balja0803 <balja0803@gmail.com>
2023-01-25 22:30:26 +0800

    b

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,15 +6,15 @@ import { useEffect, useState } from "react";
 function App() {
   const [data, setData] = useState();
   useEffect(() => {
-    axios
-      .get("http://localhost:2020/products")
-      .then((res) => setData(res.data));
+    axios.get("http://localhost:2020/products").then((res) => {
+      setData(res.data);
+    });
   }, []);
-  console.log(data);
+
   return (
     <div className="App">
       <Header />
-      <Main products={data} />
+      <Main product={data} />
     </div>
   );
 }
--- my-app/src/components/Main.jsx
+++ my-app/src/components/Main.jsx
@@ -2,14 +2,12 @@ import Home from "./subcomponents/Home";
 import Sidebar from "./subcomponents/Sidebar";
 import "../styles/main.css";
 
-export default function Main(props) {
-  const { products } = props;
-  console.log(products);
+export default function Main() {
   return (
     <div className="main">
       <Sidebar />
       <Home />
-      <p>name : {products[0].name} </p>
+      <div>s</div>
     </div>
   );
 }
