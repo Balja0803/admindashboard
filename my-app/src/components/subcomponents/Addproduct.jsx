9996820c3cb6897d0709076aa836c9d6da053bb7
balja0803 <balja0803@gmail.com>
2023-01-31 07:20:49 +0800

    b

--- my-app/src/components/subcomponents/Addproduct.jsx
+++ my-app/src/components/subcomponents/Addproduct.jsx
@@ -1,6 +1,7 @@
+import axios from "axios";
 import { useState } from "react";
 
-export default function Addproduct(props) {
+export default function Addproduct() {
   const [data, setData] = useState({
     name: "",
     price: "",
@@ -9,30 +10,74 @@ export default function Addproduct(props) {
     sale: "",
     description: "",
   });
-
+  function dataHandler(e) {
+    const newData = { ...data };
+    newData[e.target.name] = e.target.value;
+    setData(newData);
+    console.log(newData);
+  }
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(data);
+    axios.post("http://localhost:2022/products/add", {
+      name: data.name,
+      price: parseInt(data.price),
+      sale: parseInt(data.sale),
+      stock: parseInt(data.stock),
+      category: data.category,
+      description: data.description,
+    });
   };
   return (
     <div>
       <form onSubmit={submitHandler}>
         <label for="product-name">product name</label>
-        <input name="name" value={data.name} />
+        <input
+          onChange={(e) => dataHandler(e)}
+          name="name"
+          id="name"
+          value={data.name}
+        />
         <label for="price">Price</label>
-        <input type="number" name="price" value={data.price} />
+        <input
+          onChange={(e) => dataHandler(e)}
+          type="number"
+          id="price"
+          name="price"
+          value={data.price}
+        />
         <label for="stock">Stock</label>
-        <input type="text" name="stock" value={data.stock} />
-        <label for="categoy" name="category">
-          Category
-        </label>
-        <input />
+        <input
+          onChange={(e) => dataHandler(e)}
+          type="text"
+          id="stock"
+          name="stock"
+          value={data.stock}
+        />
+        <label for="category">Category</label>
+        <input
+          onChange={(e) => dataHandler(e)}
+          name="category"
+          id="category"
+          type="text"
+          value={data.category}
+        />
         <label for="sale">Sale</label>
-        <input type="number" name="sale" value={data.sale} />
+        <input
+          onChange={(e) => dataHandler(e)}
+          type="number"
+          name="sale"
+          id="sale"
+          value={data.sale}
+        />
         <label for="description">Description</label>
-        <input name="description" value={data.description} />
+        <input
+          onChange={(e) => dataHandler(e)}
+          name="description"
+          id="description"
+          value={data.description}
+        />
         <button type="submit">Submit</button>
-        <intut type="button" value="cancel" />
+        <input type="button" value="cancel" />
       </form>
     </div>
   );
