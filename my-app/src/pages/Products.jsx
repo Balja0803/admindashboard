1eb99282c83143b9150038767568cd629bb6b77e
balja0803 <balja0803@gmail.com>
2023-02-01 12:38:27 +0800

    product delete

--- my-app/src/pages/Products.jsx
+++ my-app/src/pages/Products.jsx
@@ -1,37 +1,40 @@
+import axios from "axios";
 import { useState } from "react";
 import Addproduct from "../components/subcomponents/Addproduct";
 import "../styles/products.css";
-import { Button } from "bootstrap";
-import { Offcanvas } from "react-bootstrap";
+// import { Button } from "bootstrap";
+// import { Offcanvas } from "react-bootstrap";
 
 export function Products(props) {
   const prod = props.product;
   const [showProduct, setShowProduct] = useState(false);
-  const handleClose = () => setShowProduct(false);
-  const handleShow = () => setShowProduct(true);
-  // const addProduct = () => {
-  //   if (showProduct) {
-  //     setShowProduct(false);
-  //   } else {
-  //     setShowProduct(true);
-  //   }
-  // };
+  // const handleClose = () => setShowProduct(false);
+  // const handleShow = () => setShowProduct(true);
+  const addProduct = () => {
+    if (showProduct) {
+      setShowProduct(false);
+    } else {
+      setShowProduct(true);
+    }
+  };
+  function deleteProduct(id) {
+    axios.delete(`http://localhost:2020/products/delete/${id}`);
+  }
+
   return (
     <div>
       <div>
-        <Button variant="primary" onClick={handleShow}>
-          Add Product
-        </Button>
+        <button onClick={addProduct}>Add Product</button>
 
-        <Offcanvas show={showProduct} onHide={handleClose}>
+        {/* <Offcanvas show={showProduct} onHide={handleClose}>
           <Offcanvas.Header closeButton>
             <Offcanvas.Title>Add product</Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
             <Addproduct />
           </Offcanvas.Body>
-        </Offcanvas>
-        {/* {showProduct ? <Addproduct data={prod} /> : null} */}
+        </Offcanvas> */}
+        {showProduct ? <Addproduct data={prod} /> : null}
       </div>
       <table>
         <thead>
@@ -42,6 +45,7 @@ export function Products(props) {
             <th>stock</th>
             <th>Id</th>
             <th>Category</th>
+            <th>edit/delete</th>
           </tr>
         </thead>
         <tbody>
@@ -53,6 +57,15 @@ export function Products(props) {
               <td>{item.stock}</td>
               <td>{item.id}</td>
               <td>{item.category}</td>
+              <td>
+                <button
+                  onClick={() => {
+                    deleteProduct(item.id);
+                  }}
+                >
+                  delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
