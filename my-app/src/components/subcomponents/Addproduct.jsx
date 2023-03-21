01f32f9e945471f3ecebddaa9cf50548b78118c3
balja0803 <balja0803@gmail.com>
2023-03-21 12:51:42 +0800

    new form

--- my-app/src/components/subcomponents/Addproduct.jsx
+++ my-app/src/components/subcomponents/Addproduct.jsx
@@ -1,16 +1,21 @@
 import "../../styles/addproduct.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Modal } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 
 import axios from "axios";
 
 export default function AddProduct() {
+  const [products, setProducts] = useState();
   const [file, setFile] = useState();
   const [data, setData] = useState({
     name: "",
     price: null,
-    specs: null,
+    stock: null,
+    sale: null,
+    brand: "",
+    category: "",
+    description: "",
   });
   const [show, setShow] = useState(false);
   const [formSpecs, setFormSpecs] = useState([
@@ -20,6 +25,11 @@ export default function AddProduct() {
     },
   ]);
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:2323/products")
+      .then((res) => setProducts(res.data));
+  }, []);
   const handleSpecs = (index, e) => {
     let specs = [...formSpecs];
     specs[index][e.target.name] = e.target.value;
@@ -30,59 +40,26 @@ export default function AddProduct() {
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
+
   const fileHandler = (e) => {
     console.log(e.target.files[0]);
-
-    // let files = Array.from(e.target.files);
-    const formData = new FormData();
-
-    formData.append("image", e.target.files[0]);
-
-    setFile(formData);
-
-    // Object.keys(file).forEach((key) => {
-    //   if (file[key] instanceof FileList) {
-    //     formData.append(key, file[key][0], file[key][0].name);
-    //   } else {
-    //     formData.append(key, file[key]);
-    //   }
-    // });
-
-    // console.log(files);
+    setFile(e.target.files[0]);
   };
 
   function handleSubmit(e) {
     e.preventDefault();
-
-    // let form = document.getElementById("myForm");
-    // let formData = new FormData(form);
-    // formData.append("images", e.target.files);
-    // data.specs = formSpecs;
-    // const files = e.target.images.files;
-
+    const formData = new FormData();
+    formData.append("image", file);
+    formData.append("body", JSON.stringify(data));
+    formData.append("specs", JSON.stringify(formSpecs));
     axios({
       method: "POST",
       url: "http://localhost:2323/products/add",
-      data: file,
+      data: formData,
       headers: {
         "Content-Type": "multipart/form-data",
       },
     }).then((res) => console.log(res));
-
-    // fetch("http://localhost:2323/products/add", {
-    //   method: "POST",
-    //   body: formData,
-    // }).then((res) => console.log(res));
-
-    // formData.append("body", JSON.stringify(data));
-    // formData.append("body", data);
-    // for (let i = 0; i < file.filelist.length; i++) {
-    //   formData.append("images", file.filelist.file[i]);
-    // }
-
-    // axios
-    //   .post("http://localhost:2323/products/add", formData)
-    //   .then(() => console.log("posted"));
   }
 
   const handleClose = () => setShow(false);
@@ -114,6 +91,19 @@ export default function AddProduct() {
             placeholder="price"
             onChange={handleChange}
           />
+          <input
+            type="number"
+            name="stock"
+            placeholder="stock"
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            name="sale"
+            placeholder="sale"
+            onChange={handleChange}
+          />
+
           <br />
           <label htmlFor="specs"> Specs</label>
           {formSpecs.map((spec, index) => (
@@ -130,7 +120,26 @@ export default function AddProduct() {
               />
             </div>
           ))}
-
+          <input
+            type="text"
+            name="brand"
+            placeholder="brand"
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="category"
+            placeholder="category"
+            onChange={handleChange}
+          />
+          <input
+            id="description"
+            type="textarea"
+            rows="2"
+            name="description"
+            placeholder="description"
+            onChange={handleChange}
+          />
           <input
             type="file"
             name="image"
@@ -141,6 +150,22 @@ export default function AddProduct() {
           <input type="submit" onClick={handleSubmit} />
         </form>
       </Modal>
+
+      <div>
+        {products &&
+          products.map((product, i) => (
+            <div key={i}>
+              <img
+                alt="sample"
+                src={product.image[0]}
+                width="300"
+                height="250"
+              />
+              <p>name : {product.name}</p>
+              <p>price : {product.price}</p>
+            </div>
+          ))}
+      </div>
     </>
   );
 }
--- my-app/src/components/subcomponents/EditProduct.jsx
+++ my-app/src/components/subcomponents/EditProduct.jsx
@@ -4,33 +4,6 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function EditProduct() {
-  const [data, setData] = useState();
-  useEffect(() => {
-    axios
-      .get("http://localhost:2323/products")
-      .then((res) => setData(res.data));
-  }, []);
-
-  // const base64String = btoa(
-  //   String.fromCharCode(...new Uint8Array(data && data.image.data.data))
-  // );
-  console.log(data);
-  return (
-    <div>
-      {data &&
-        data[16].image.map((img, i) => {
-          const base64String = btoa(
-            String.fromCharCode(...new Uint8Array(img.data.data))
-          );
-          return (
-            <img
-              key={i}
-              alt="sample"
-              src={`data:image/png; base64, ${base64String}`}
-            />
-          );
-        })}
-    </div>
-  );
+  return <div>order</div>;
 }
 export default EditProduct;
