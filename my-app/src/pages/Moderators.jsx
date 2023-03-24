6ccba8b14751cf087ce7444cbae9287b1656b84b
balja0803 <balja0803@gmail.com>
2023-03-24 12:45:25 +0800

    q

--- my-app/src/pages/Moderators.jsx
+++ my-app/src/pages/Moderators.jsx
@@ -1,5 +1,28 @@
-import Button from "react-bootstrap/Button";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export function Moderators() {
-  return <div>mods</div>;
+  const [products, setProducts] = useState();
+  useEffect(() => {
+    axios
+      .get("http://localhost:2323/products")
+      .then((res) => setProducts(res.data));
+  }, []);
+
+  return (
+    <div>
+      {products ? (
+        <div>
+          {products.map((product, i) => (
+            <div key={i}>
+              <p>name: {product.name}</p>
+              <p>category: {product.category.name}</p>
+              <p>brand: {product.brand.name}</p>
+              <img alt="sample" src={product.image[0]} />
+            </div>
+          ))}
+        </div>
+      ) : null}
+    </div>
+  );
 }
