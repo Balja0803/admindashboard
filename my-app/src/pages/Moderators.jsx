48ebe067bbd4678d35127441f4a917d9b9234646
balja0803 <balja0803@gmail.com>
2023-03-27 13:00:25 +0800

    yuna rest

--- my-app/src/pages/Moderators.jsx
+++ my-app/src/pages/Moderators.jsx
@@ -2,27 +2,43 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export function Moderators() {
-  const [products, setProducts] = useState();
-  useEffect(() => {
-    axios
-      .get("http://localhost:2323/products")
-      .then((res) => setProducts(res.data));
-  }, []);
+  const [rest, setRest] = useState();
+  const location = {
+    location: {
+      type: "point",
+      coordinates: [106.933868, 47.9237483],
+    },
+  };
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+  function showPosition(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log("Latitude: " + latitude);
+    console.log("Longitude: " + longitude);
+  }
+
+  const fetchLocation = async (location) => {
+    // const formData = new FormData();
+    // formData.append("location", JSON.stringify(location));
+    const result = await axios.post(
+      "http://localhost:2323/restaurants/nearest",
+      location
+    );
+    console.log(result.data);
+    setRest(result.data);
+  };
 
   return (
     <div>
-      {products ? (
-        <div>
-          {products.map((product, i) => (
-            <div key={i}>
-              <p>name: {product.name}</p>
-              <p>category: {product.category.name}</p>
-              <p>brand: {product.brand.name}</p>
-              <img alt="sample" src={product.image[0]} />
-            </div>
-          ))}
-        </div>
-      ) : null}
+      <button onClick={() => fetchLocation(location)}>
+        Find nearest restaurants
+      </button>
+      {rest && <p>Nearest restaurants: {rest.name}</p>}
     </div>
   );
 }
